a88f552ecada5c16bb8983d0b2855be1e5c56e6d
caojunjie <1301239018@qq.com>
2021-12-20 15:16:17 +0800

    webworker

--- webworker/server/sw.js
+++ webworker/server/sw.js
@@ -1,23 +1,56 @@
+const cacheData = [
+  '/',
+  '/index.html',
+  '/404.html',
+  '/images/all.png',
+  '/images/logo.png',
+  '/sw.js'
+];
+
+const cacheName = 'static-v1';
+
 self.addEventListener("install", (event) => {
   console.log("V1 installing…");
 
-  // cache a cat SVG
-//   event.waitUntil(
-//     caches.open("static-v1").then((cache) => cache.add("/cat.svg"))
-//   );
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        // cache.add("/cat.svg");
+        return cache.addAll(cacheData);
+      })
+  );
 });
 
 self.addEventListener("activate", (event) => {
   console.log("V1 now ready to handle fetches!");
 });
 
-/* self.addEventListener("fetch", (event) => {
-  const url = new URL(event.request.url);
+self.addEventListener("fetch", (event) => {
+  // const url = new URL(event.request.url);
 
-  // serve the cat SVG from the cache if the request is
-  // same-origin and the path is '/dog.svg'
-  if (url.origin == location.origin && url.pathname == "/dog.svg") {
-    event.respondWith(caches.match("/cat.svg"));
-  }
+  // if (url.origin == location.origin && url.pathname == "/dog.svg") {
+  //   event.respondWith(caches.match("/cat.svg"));
+  // }
+
+
+  event.respondWith(caches.match(event.request).then(function(response) {
+
+    if(response !== undefined) {
+      return response;
+    }else {
+      fetch(event.request).then(function(response) {
+        let responseClone = response.clone();
+
+        caches.open(cacheName).then(function (cache) {
+          cache.put(event.request, responseClone);
+        })
+
+        return response;
+      }).catch(function() {
+        return caches.match('/404.html');
+      })
+    }
+
+  }))
+
 });
- */
