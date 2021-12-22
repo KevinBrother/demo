5f1a93f4a7eb93b210779af8a9328d9296e15fd3
caojunjie <1301239018@qq.com>
2021-12-22 14:58:26 +0800

    service的依赖注入、消息模块

--- packages/angular-study/src/app/heroes/heroes.component.ts
+++ packages/angular-study/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +11,20 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
-  constructor() {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getHeros();
+  }
   onSelect(hero: Hero) {
     this.selectedHero =hero;
+    this.messageService.add(`HeroesComponent : Selected hero id = ${hero.id}`)
+  }
+
+  getHeros(): void {
+    this.heroService.getHeros()
+      .subscribe(heros => this.heroes = heros);
   }
 }
